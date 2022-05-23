96e5dc7124392ed5cbf69bd002f6f3df0d85b3f8
SinZ <github@361zn.is>
2022-05-23 23:05:49 +1000

    Release Profiler v1.0

---
+++ Profiler/ModConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Profiler
+{
+    internal class ModConfig
+    {
+        public int BigLoopThreshold { get; set; } = 100;
+        public int EventThreshold { get; set; } = 10;
+    }
+}
--- Profiler/ModEntry.cs
+++ Profiler/ModEntry.cs
@@ -11,10 +11,12 @@ namespace Profiler
 {
     public class ModEntry : Mod
     {
+        internal ModConfig Config { get; private set; }
         public Stopwatch timer { get; private set; }
 
         public override void Entry(IModHelper helper)
         {
+            this.Config = this.Helper.ReadConfig<ModConfig>();
             this.timer = new Stopwatch();
             helper.Events.GameLoop.SaveLoaded += GameLoop_SaveLoadedFast;
             helper.Events.GameLoop.DayStarted += GameLoop_DayStartedFast;
@@ -24,55 +26,57 @@ namespace Profiler
             helper.Events.GameLoop.DayStarted += GameLoop_DayStartedSlow;
             helper.Events.GameLoop.GameLaunched += GameLoop_GameLaunchedSlow;
             helper.Events.Specialized.LoadStageChanged += Specialized_LoadStageChangedSlow;
+
+            ManagedEventPatches.Initialize(Monitor, this.Config, new HarmonyLib.Harmony(this.ModManifest.UniqueID));
             this.timer.Restart();
         }
 
-        [EventPriority(EventPriority.High + 1337)]
+        [EventPriority((EventPriority)Int32.MaxValue)]
         private void Specialized_LoadStageChangedFast(object sender, StardewModdingAPI.Events.LoadStageChangedEventArgs e)
         {
-            Monitor.Log($"[{timer.ElapsedMilliseconds:N6}][Fast] LoadStageChanged {e.OldStage} -> {e.NewStage}", LogLevel.Info);
+            Monitor.Log($"[{timer.Elapsed.TotalMilliseconds:N}][Fast] LoadStageChanged {e.OldStage} -> {e.NewStage}", LogLevel.Info);
         }
 
-        [EventPriority(EventPriority.High + 1337)]
+        [EventPriority((EventPriority)Int32.MaxValue)]
         private void GameLoop_GameLaunchedFast(object sender, StardewModdingAPI.Events.GameLaunchedEventArgs e)
         {
-            Monitor.Log($"[{timer.ElapsedMilliseconds:N6}][Fast] Game Launched", LogLevel.Info);
+            Monitor.Log($"[{timer.Elapsed.TotalMilliseconds:N}][Fast] Game Launched", LogLevel.Info);
         }
 
-        [EventPriority(EventPriority.High + 1337)]
+        [EventPriority((EventPriority)Int32.MaxValue)]
         private void GameLoop_DayStartedFast(object sender, StardewModdingAPI.Events.DayStartedEventArgs e)
         {
-            Monitor.Log($"[{timer.ElapsedMilliseconds:N6}][Fast] Day Started", LogLevel.Info);
+            Monitor.Log($"[{timer.Elapsed.TotalMilliseconds:N}][Fast] Day Started", LogLevel.Info);
         }
 
-        [EventPriority(EventPriority.High + 1337)]
+        [EventPriority((EventPriority)Int32.MaxValue)]
         private void GameLoop_SaveLoadedFast(object sender, StardewModdingAPI.Events.SaveLoadedEventArgs e)
         {
-            Monitor.Log($"[{timer.ElapsedMilliseconds:N6}][Fast] Save Loaded", LogLevel.Info);
+            Monitor.Log($"[{timer.Elapsed.TotalMilliseconds:N}][Fast] Save Loaded", LogLevel.Info);
         }
 
-        [EventPriority(EventPriority.Low - 1337)]
+        [EventPriority((EventPriority)Int32.MinValue)]
         private void Specialized_LoadStageChangedSlow(object sender, StardewModdingAPI.Events.LoadStageChangedEventArgs e)
         {
-            Monitor.Log($"[{timer.ElapsedMilliseconds:N6}][Slow] LoadStageChanged {e.OldStage} -> {e.NewStage}", LogLevel.Info);
+            Monitor.Log($"[{timer.Elapsed.TotalMilliseconds:N}][Slow] LoadStageChanged {e.OldStage} -> {e.NewStage}", LogLevel.Info);
         }
 
-        [EventPriority(EventPriority.Low - 1337)]
+        [EventPriority((EventPriority)Int32.MinValue)]
         private void GameLoop_GameLaunchedSlow(object sender, StardewModdingAPI.Events.GameLaunchedEventArgs e)
         {
-            Monitor.Log($"[{timer.ElapsedMilliseconds:N6}][Slow] Game Launched", LogLevel.Info);
+            Monitor.Log($"[{timer.Elapsed.TotalMilliseconds:N}][Slow] Game Launched", LogLevel.Info);
         }
 
-        [EventPriority(EventPriority.Low - 1337)]
+        [EventPriority((EventPriority)Int32.MinValue)]
         private void GameLoop_DayStartedSlow(object sender, StardewModdingAPI.Events.DayStartedEventArgs e)
         {
-            Monitor.Log($"[{timer.ElapsedMilliseconds:N6}][Slow] Day Started", LogLevel.Info);
+            Monitor.Log($"[{timer.Elapsed.TotalMilliseconds:N}][Slow] Day Started", LogLevel.Info);
         }
 
-        [EventPriority(EventPriority.Low - 1337)]
+        [EventPriority((EventPriority)Int32.MinValue)]
         private void GameLoop_SaveLoadedSlow(object sender, StardewModdingAPI.Events.SaveLoadedEventArgs e)
         {
-            Monitor.Log($"[{timer.ElapsedMilliseconds:N6}][Slow] Save Loaded", LogLevel.Info);
+            Monitor.Log($"[{timer.Elapsed.TotalMilliseconds:N}][Slow] Save Loaded", LogLevel.Info);
         }
     }
 }
