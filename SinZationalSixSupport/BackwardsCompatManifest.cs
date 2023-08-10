be316c1f65c6680ad72a37ef59a5beb08eac7b12
SinZ <github@361zn.is>
2023-08-10 10:09:23 +1000

    Misc 1.6 Debug mods WIP

---
+++ SinZationalSixSupport/BackwardsCompatManifest.cs
@@ -0,0 +1,42 @@
+﻿using StardewModdingAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinZationalSixSupport
+{
+    internal class BackwardsCompatManifest : IManifest
+    {
+        public BackwardsCompatManifest(string uniqueID, string name, string description, string author, ISemanticVersion version) 
+        {
+            this.UniqueID = uniqueID;
+            this.Name = name;
+            this.Description = description;
+            this.Author = author;
+            this.Version = version;
+        }
+
+        public string Name { get; internal init; }
+
+        public string Description { get; internal init; }
+
+        public string Author { get; internal init; }
+
+        public ISemanticVersion Version { get; internal init; }
+
+        public ISemanticVersion MinimumApiVersion => null;
+        public string UniqueID { get; internal init; }
+
+        public string EntryDll => "";
+
+        public IManifestContentPackFor ContentPackFor => null;
+
+        public IManifestDependency[] Dependencies => Array.Empty<IManifestDependency>();
+
+        public string[] UpdateKeys => Array.Empty<string>();
+
+        public IDictionary<string, object> ExtraFields => new Dictionary<string, object>();
+    }
+}
